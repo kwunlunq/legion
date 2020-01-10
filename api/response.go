658292eb12119bec88bb2aea8b0ce4fce7069b72
george_liu <george_liu@paradise-soft.com.tw>
2020-01-10 18:22:52 +0800

    update api log

--- api/response.go
+++ api/response.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"gitlab.paradise-soft.com.tw/glob/tracer"
@@ -36,4 +37,32 @@ func responseParamError(ctx *gin.Context, err error) {
 		Message: fmt.Sprintf("參數錯誤: %v", err.Error()),
 	}
 	ctx.JSON(http.StatusBadRequest, resp)
+}
+
+func logFormat(param gin.LogFormatterParams) string {
+	var statusColor, methodColor, resetColor string
+	if param.IsOutputColor() {
+		statusColor = param.StatusCodeColor()
+		methodColor = param.MethodColor()
+		resetColor = param.ResetColor()
+	}
+
+	if param.Latency > time.Minute {
+		// Truncate in a golang < 1.8 safe way
+		param.Latency = param.Latency - param.Latency%time.Second
+	}
+	if param.StatusCode != http.StatusOK {
+		return fmt.Sprintf("[GIN] %v |%s %3d %s| %13v | %15s |%s %-7s %s %s\n%s",
+			param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+			statusColor, param.StatusCode, resetColor,
+			param.Latency,
+			param.ClientIP,
+			methodColor, param.Method, resetColor,
+			param.Path,
+			param.ErrorMessage,
+		)
+	} else {
+		return ""
+	}
+
 }
