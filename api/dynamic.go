a729000991d2b92f1a124e842b7371fb1305f512
george_liu <george_liu@paradise-soft.com.tw>
2020-01-21 14:19:58 +0800

    update log

--- api/dynamic.go
+++ api/dynamic.go
@@ -25,7 +25,7 @@ func dynamicScrape(data []byte) (err error) {
 
 	now := time.Now()
 	if legionReq.SentAt.IsZero() || legionReq.SentAt.After(now) || legionReq.SentAt.Add(service.ExpiredTime).Before(now) {
-		err = fmt.Errorf("task expired sent at %v", legionReq.SentAt)
+		err = fmt.Errorf("task expired sent at %v(now: %v)", legionReq.SentAt, now)
 		return
 	}
 
