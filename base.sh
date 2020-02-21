0884870062aad9d8a86d04340462c024867f33a9
george_liu <george_liu@paradise-soft.com.tw>
2020-02-21 14:13:45 +0800

    [update] 增加build的base

--- base.sh
+++ base.sh
@@ -3,3 +3,11 @@
 docker build . -f Dockerfile.base -t reg.paradise-soft.com.tw:5000/base-legion:latest
 
 docker push reg.paradise-soft.com.tw:5000/base-legion:latest
+
+docker rmi reg.paradise-soft.com.tw:5000/base-legion:latest
+
+docker build . -f Dockerfile.build.base -t reg.paradise-soft.com.tw:5000/base-build-legion:latest
+
+docker push reg.paradise-soft.com.tw:5000/base-build-legion:latest
+
+docker rmi reg.paradise-soft.com.tw:5000/base-build-legion:latest
